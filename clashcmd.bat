a14b6ec6d7eb40c23e97cd7750c602848d165489
zhongfly <icesshadows@gmail.com>
2020-07-24 19:25:22 +0800

    use GeoIP MaxMind Database with china ip list
    https://github.com/alecthw/mmdb_china_ip_list

--- clashcmd.bat
+++ clashcmd.bat
@@ -651,11 +651,8 @@ echo !loc_advanced_geoip_updating!
 echo.
 echo -------------------------------------
 echo.
-curl -kLo GeoLite2-Country.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=oeEqpP5QI21N&suffix=tar.gz"
-7za.exe e GeoLite2-Country.tar.gz
-7za e GeoLite2-Country.tar "GeoLite2-Country*\GeoLite2-Country.mmdb" -aoa
+curl -kLo GeoLite2-Country.mmdb "https://cdn.jsdelivr.net/gh/alecthw/mmdb_china_ip_list@release/Country.mmdb"
 del "Country.mmdb" /f /q
-del "GeoLite2-Country.tar*" /f /q
 ren GeoLite2-Country.mmdb Country.mmdb
 echo.
 echo -------------------------------------
